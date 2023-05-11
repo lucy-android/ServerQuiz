d9d6dd76a987d6d8c84b0d55775d9302334e1b99
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2023-05-11 20:22:26 +0300

    Added endpoint to refresh the token

--- server.js
+++ server.js
@@ -46,6 +46,17 @@ app.post('/login', async (req, res) => {
     }
 })
 
+app.post('/refreshtoken', (req, res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken.toString(), process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+      if (err) return res.sendStatus(403)
+      const accessToken = generateAccessToken({ name: user.name })
+      res.json({ accessToken: accessToken })
+    })
+  })
+
 function generateAccessToken(user) {
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' })
 }
