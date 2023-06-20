70a745ab633d19dac59bdb724ff5a098f51bfb55
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2023-06-20 11:26:36 +0300

    CU-8: created the method for user registration

--- server.js
+++ server.js
@@ -23,24 +23,42 @@ const connection = mysql.createConnection({
 
 app.post('/register', async (req, res) => {
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        const user = { name: req.body.name, password: hashedPassword }
-        // var values = [connection.escape(req.body.name)];
-        // TODO I will start from out the next time
-        // TODO to figure out how to execute valid sql requests with data obtained from code.
-        let sql = "INSERT INTO users (firstname, lastname, login, hashedPassword) VALUES (\"Robin\", \"Hood\", \"robby\", \"12345678\")";
+        /*
+           At first I need to check whether a user with such login already exists.
+        */
 
-        connection.query(sql, (err, result) => {
+        let existLoginSql = `SELECT * FROM users WHERE login=\"${req.body.login}\"`;
+
+        connection.query(existLoginSql, (err, result) => {
             if (err) throw err;
             console.log(result);
+
+            if (result.length != 0) {
+                console.log("User with such login already exists!");
+                res.status(400).send("User with such login already exists!")
+            }
         });
 
-        if (users.find(user => user.name === req.body.name)) {
-            res.status(400).json(constants.USER_EXISTS).send()
-        } else {
-            users.push(user)
-            res.status(201).json({ name: user.name, hashedPassword }).send()
-        }
+        /*
+          if we arrive here, it means that the user has unique login
+        */
+
+        // here we hash the password provided for registration
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+        // create a new user
+
+        const user = { firstname: req.body.firstname, lastname: req.body.lastname, login: req.body.login, password: hashedPassword }
+
+        // and now we execute an sql statement to insert the new user into the users table
+        let insertSql = `INSERT INTO users (firstname, lastname, login, hashedPassword) 
+           VALUES ("${user.firstname}", "${user.lastname}", "${user.login}", "${user.password}")`;
+
+        connection.query(insertSql, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.status(201).json({ firstname: user.firstname, lastname: user.lastname, login: user.login }).send()
+        });
     } catch {
         res.status(500).send()
     }
@@ -50,20 +68,20 @@ app.post('/login', async (req, res) => {
     const user = users.find(user => user.name === req.body.name)
     if (user == null) {
         return res.status(400).send(constants.USER_NOT_EXISTS)
-    }
+        }
 
-    try {
-        const accessToken = generateAccessToken(user)
-        const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+        try {
+                const accessToken = generateAccessToken(user)
+                const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
         refreshTokens.push(refreshToken)
         if (await bcrypt.compare(req.body.password, user.password)) {
-            res.status(200).send({ accessToken: accessToken, refreshToken: refreshToken })
-        } else {
-            res.status(401).send(constants.NOT_ALLOWED)
+                    res.status(200).send({ accessToken: accessToken, refreshToken: refreshToken })
+            } else {
+                res.status(401).send(constants.NOT_ALLOWED)
+            }
+        } catch {
+            res.status(500).send()
         }
-    } catch {
-        res.status(500).send()
-    }
 })
 
 app.post('/refreshtoken', (req, res) => {
